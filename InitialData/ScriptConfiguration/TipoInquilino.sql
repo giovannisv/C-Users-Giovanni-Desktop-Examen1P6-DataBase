fe65b80fae8ca00cd588b12bb1a3c7bcd36147f8
giovannisv <geoseve@hotmail.com>
2021-10-23 07:19:01 -0600

    actualizartodo

---
+++ InitialData/ScriptConfiguration/TipoInquilino.sql
@@ -0,0 +1,31 @@
+﻿DROP TABLE IF EXISTS #TipoInqulinoTemp
+
+SELECT 
+Fisico, Juridico  INTO #TipoInquilinoTemp
+FROM (
+VALUES
+(1, 'Fisico'),
+(2, 'Juridico')
+
+
+)AS TEMP (Fisico, Juridico)
+
+
+----ACTUALIZAR DATOS---
+UPDATE P SET
+  P.Fisico= TM.Fisico,
+  P.Juridico= TM.Juridico
+FROM dbo.TipoInquilino P
+INNER JOIN #TipoInquilinoTemp TM
+    ON P.Id_TipoInquilino= TM.Id_TipoInquilino
+
+
+----INSERTAR DATOS---
+
+SET IDENTITY_INSERT dbo.TipoInquilino ON
+
+INSERT INTO dbo.TipoInquilino(
+Fisico,Juridico)
+SELECT
+Fisico,Juridico
+FROM #TipoInquilinoTemp
---
+++ WBL/InquilinoService.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BD;
+using Entity;
+
+namespace WBL
+{
+    public interface IInquilinoService
+    {
+        Task<DBEntity> Create(InquilinoEntity entity);
+        Task<DBEntity> Delete(InquilinoEntity entity);
+        Task<IEnumerable<InquilinoEntity>> Get();
+        Task<InquilinoEntity> GetById(InquilinoEntity entity);
+        Task<DBEntity> Update(InquilinoEntity entity);
+    }
+
+    public class InquilinoService : IInquilinoService
+    {
+        private readonly IDataAcces sql;
+
+        public InquilinoService(IDataAcces _sql)
+        {
+            sql = _sql;
+        }
+
+        #region Metodos Crud
+        //Metodo Get
+        public async Task<IEnumerable<InquilinoEntity>> Get()
+        {
+            try
+            {
+                var result = sql.QueryAsync<InquilinoEntity>("dbo.InquilinoObtener");
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        //Metodo GetById
+        public async Task<InquilinoEntity> GetById(InquilinoEntity entity)
+        {
+            try
+            {
+                var result = sql.QueryFirstAsync<InquilinoEntity>("dbo.InquilinoObtener", new
+                { entity.Id_TipoInquilino });
+                return await result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        //Metodo create
+        public async Task<DBEntity> Create(InquilinoEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("dbo.InquilinoInsertar", new
+                {
+                    entity.Descripcion,
+                    entity.Estado
+                    
+                });
+                return await result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<DBEntity> Update(InquilinoEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("dbo.InquilinoActualizar", new
+                {
+                    entity.Id_TipoInquilino,
+                    entity.Descripcion,
+                    entity.Estado
+                    
+                });
+                return await result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public async Task<DBEntity> Delete(InquilinoEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("dbo.InquilinoEliminar", new
+                {
+                    entity.Id_TipoInquilino,
+
+                });
+                return await result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        #endregion
+
+    }
+}
+
